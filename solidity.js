e0100ae3ac5f2b4fd6a6c96a0d3487a727c8fed1
Harry Altman <harry.j.altman@gmail.com>
2019-06-10 22:21:49 -0400

    Move logging primitives above "not really toplevel" note

--- solidity.js
+++ solidity.js
@@ -85,10 +85,10 @@ function hljsDefineSolidity(hljs) {
             'blockhash gasleft ' +
             'assert revert require ' +
             'sha3 sha256 keccak256 ripemd160 ecrecover addmod mulmod ' +
+            'log0 log1 log2 log3 log4' +
             // :NOTE: not really toplevel, but advantageous to have highlighted as if reserved to
             //        avoid newcomers making mistakes due to accidental name collisions.
-            'send transfer call callcode delegatecall staticcall ' +
-            'log0 log1 log2 log3 log4',
+            'send transfer call callcode delegatecall staticcall ',
     };
 
     //like a C number, except:
